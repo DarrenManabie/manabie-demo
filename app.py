c6f196b5ab339dc17a80c20b2885399d3921268c
DarrenManabie <118432717+DarrenManabie@users.noreply.github.com>
2024-08-28 11:55:50 +0900

    Update app.py

--- app.py
+++ app.py
@@ -24,17 +24,10 @@ HARDCODED_QUESTION = """
                     ## Section16, 平方根の意味と混合, Practice, STEP1, null, (1), 次の数の平方根を求めなさい。, ①, 4, FIB, null, I-No
                     ## Section16, 平方根の意味と混合, Practice, STEP1, null, (1), 次の数の平方根を求めなさい。, ②, 16, FIB, null, I-No
                     ## Section17, 平方根の大小, Practice, STEP2, 次の各組の数の大小を、不等号を使って表しなさい。，(1), \sqrt{7}、\sqrt{6}, FIB, null, I-No
-                    #「問題セクション名」は、Practice, Brush Up, まとめ, まとめB の4種類を抜け漏れなく全て書き出して。Study, Checkは出力しないで
+                    
                     #「回答形式」は「FIB, MCQ, MAQ」の3種類のみです。"FIB"は空欄補充形式、"MCQ"は単一回答の選択式（正答が1つのみ）、"MAQ"は複数回答の選択式（正答が2つ以上）です
                     #「回答オプション」は、FIBの問題には必ず"null"を表記して。MCQ or MAQの問題には、全ての選択肢の内容をカンマで区切って表記して。
                     """
-
-# Get user input (optional)
-user_input = st.text_area("追加の質問を入力してください（オプション）:")
-
-# Concatenate user input to the hardcoded question if provided
-if user_input:
-    HARDCODED_QUESTION += "\n" + user_input
 
 def process_pdf(uploaded_file):
     # Save the uploaded file temporarily
@@ -54,6 +47,13 @@ def process_pdf(uploaded_file):
 
 # Streamlit app
 st.title("教科書からの問題抽出デモ")
+
+# Get user input (optional)
+user_input = st.text_area("追加の質問を入力してください（オプション）:")
+
+# Concatenate user input to the hardcoded question if provided
+if user_input:
+    HARDCODED_QUESTION += "\n" + user_input
 
 # File uploader
 uploaded_file = st.file_uploader("", type="pdf")
@@ -76,3 +76,6 @@ if uploaded_file is not None:
             st.write("処理が完了しました。")
 else:
     st.write("PDFファイルをアップロードしてください。")
+
+
+
